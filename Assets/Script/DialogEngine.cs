03e2f1704da3326b756a0ae5172c1b3381f3400b
OtakuEmperor <oe@nisra.net>
2017-11-23 00:34:18 +0800

    init project with DialogEngine

---
+++ Assets/Script/DialogEngine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class DialogEngine : MonoBehaviour {
+
+	public Text DialogText;
+
+	// Use this for initialization
+	void Start () {
+		DialogText.text = "Fuck myself";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick(){
+		DialogText.text = "Fuck yourself";
+	}
+}
